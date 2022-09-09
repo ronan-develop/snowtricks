8b411c28fac938d62022be0dcebf9ee4c518992f
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-09 08:59:52 +0200

    Feat: Trick data on details page controller get data and pass theme to twig Files: - src/Controller/TrickController.php - templates/trick/index.html.twig

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TrickRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrickController extends AbstractController
 {
-    #[Route('/trick', name: 'app_trick')]
-    public function index(): Response
+    #[Route('/trick/{slug}', name: 'app_trick')]
+    public function index(TrickRepository $trickRepository, Request $request): Response
     {
+        $slug = $request->get('slug');
+        $trick = $trickRepository->findOneBy(['slug'=>$slug]);
+        
         return $this->render('trick/index.html.twig', [
-            'controller_name' => 'TrickController',
+            'trick' => $trick,
         ]);
     }
 }
