005630c1591c6157aba18c4d53ca52bc85f69f53
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-09 10:18:35 +0200

    Feat: EasyAdmin customization Fixes #4 activate traduction, customization on fields Files: - config/packages/translation.yaml: traduction - src/Controller/Admin/CategoryCrudController.php: create & customize fields - src/Controller/Admin/DashboardController.php: Configure menu & actions - src/Controller/Admin/TrickCrudController.php: create & customize fields

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,34 +2,33 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\Trick;
+use App\Entity\Category;
+use App\Controller\Admin\TrickCrudController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        $url = $this->adminUrlGenerator->setController(TrickCrudController::class)
+        ->generateUrl();
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
@@ -40,7 +39,30 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::section('Navigation');
+        yield MenuItem::subMenu('', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToDashboard('panneau admin', 'fa-solid fa-dashboard'),
+            MenuItem::linkToUrl('retour au site', 'fa-solid fa-house', $this->generateUrl('app_home')),
+        ]);
+        
+        yield MenuItem::section('Categories');
+        yield MenuItem::subMenu('', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('ajout d\'une catégorie', 'fas fa-plus', Category::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Voir les catégories', 'fas fa-eye', Category::class),
+        ]);
+
+        yield MenuItem::section('Tricks');
+        yield MenuItem::subMenu('', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('ajout d\'un trick', 'fas fa-plus', Trick::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Voir les Tricks', 'fas fa-eye', Trick::class),
+        ]);
+
     }
+
+    public function configureActions():Actions
+    {
+        return parent::configureActions()
+        ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
 }
