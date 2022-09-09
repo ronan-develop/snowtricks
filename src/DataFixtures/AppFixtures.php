09730ccf0f6958d18b79309f436b00c242cfef0d
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-09 08:15:53 +0200

    Feat: trick card on homepage copay paste files from the original repo _p6_snowtricks Files: - assets/img/regular.jpg - src/DataFixtures/AppFixtures.php - templates/components/_trick.html.twig - templates/home/index.html.twig

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,30 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Category;
+use DateTimeZone;
+use App\Entity\Trick;
+use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use PhpParser\Node\Stmt\Catch_;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $category = new Category();
+        $category->setName('Stances');
+        $manager->persist($category);
 
+        $trick = new Trick();
+        $trick->setCreatedAt(
+            new DateTimeImmutable("now", new DateTimeZone('Europe/Paris'))
+        )->setName('Regular')
+        ->setDescription("Rider avec le pied gauche devant dans sa position naturelle")
+        ->setImage("/public/uploads/regular.jpg");
+        $manager->persist($trick);
+        
         $manager->flush();
     }
 }
