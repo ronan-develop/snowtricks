5ae1946ae3f00a9e29e6e93ee0af52cb681bd61f
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-12 15:44:44 +0200

    Feat: account can find tricks by users Verif User, ask repo by user pass results in twig Files: - src/Controller/AccountController.php

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\TrickRepository;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountController extends AbstractController
 {
     #[Route('/account', name: 'app_account')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_profile');
+        }
+
+        $user = $userRepository->findOneBy([
+            'username' => $this->getUser()->getUserIdentifier()
+        ]);
+
+        $tricks = $user->getTricks();
+
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'tricks' => $tricks,
+            'user_name' => $user->getUsername()
         ]);
     }
 }
