1a77666f11a931255bb55cc99653fb4979e4216b
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-12 14:03:45 +0200

    Feat: trick & images relations creating relation for images collection Files: - migrations/Version20220912085454.php - migrations/Version20220912114502.php - migrations/Version20220912114637.php - migrations/Version20220912114728.php - src/Entity/Image.php - src/Entity/Trick.php - src/Repository/ImageRepository.php

---
+++ src/Entity/Image.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\ManyToOne(inversedBy: 'images')]
+    private ?Trick $trick = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $is_featured = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTrick(): ?Trick
+    {
+        return $this->trick;
+    }
+
+    public function setTrick(?Trick $trick): self
+    {
+        $this->trick = $trick;
+
+        return $this;
+    }
+
+    public function isFeatured(): ?bool
+    {
+        return $this->is_featured;
+    }
+
+    public function setFeatured(?bool $is_featured): self
+    {
+        $this->is_featured = $is_featured;
+
+        return $this;
+    }
+}
--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -22,8 +22,6 @@ class Trick
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $image = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $video = null;
@@ -43,9 +41,13 @@ class Trick
     #[ORM\ManyToOne(inversedBy: 'tricks')]
     private ?User $owner = null;
 
+    #[ORM\OneToMany(mappedBy: 'trick', targetEntity: Image::class)]
+    private Collection $images;
+
     public function __construct()
     {
         $this->category = new ArrayCollection();
+        $this->images = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -73,18 +75,6 @@ class Trick
     public function setDescription(string $description): self
     {
         $this->description = $description;
-
-        return $this;
-    }
-
-    public function getImage(): ?string
-    {
-        return $this->image;
-    }
-
-    public function setImage(?string $image): self
-    {
-        $this->image = $image;
 
         return $this;
     }
@@ -169,6 +159,36 @@ class Trick
     public function setOwner(?User $owner): self
     {
         $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Image>
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images->add($image);
+            $image->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getTrick() === $this) {
+                $image->setTrick(null);
+            }
+        }
 
         return $this;
     }
