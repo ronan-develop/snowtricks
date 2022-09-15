279f515ca650767fa8801991bd37b791d5722bc0
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-15 15:32:56 +0200

    Feat: set actions & configure fields user-dependent instruction Files: - src/Controller/Admin/TrickCrudController.php

--- src/Controller/Admin/TrickCrudController.php
+++ src/Controller/Admin/TrickCrudController.php
@@ -3,7 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Trick;
+use App\Entity\User;
 use App\Repository\TrickRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -31,66 +35,83 @@ class TrickCrudController extends AbstractCrudController
     {
     }
 
+
     public static function getEntityFqcn(): string
     {
         return Trick::class;
     }
 
+    public function redirectToLogin()
+    {
+        $this->redirectToRoute('app_login');
+        return $this;
+    }
+
     public function configureActions(Actions $actions): Actions
     {
+
         if (!$this->isGranted('ROLE_ADMIN') && $this->getUser()->getId() !== $this->trickRepository->findBy(['id' => $this->getUser()->getId()])) {
             return $actions
             ->remove(Crud::PAGE_INDEX, Action::DELETE)
             ->remove(Crud::PAGE_INDEX, Action::EDIT)
             ->remove(Crud::PAGE_DETAIL, Action::DELETE)
             ->remove(Crud::PAGE_DETAIL, Action::EDIT)
+            ->remove(Crud::PAGE_EDIT, Action::SAVE_AND_CONTINUE)
+            ->remove(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN)
             ;
         }
-            return $actions
-            ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
-                return $action->setIcon('fa-fw fas fa-person-snowboarding')->setLabel("Nouveau trick");
-            })
-            ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
-                return $action
-                ->setIcon('fa-solid fa-gears');
-            })
-            ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
-                return $action
-                ->setIcon('fa-solid fa-trash');
-            })
-            ->update(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER, function (Action $action) {
-                return $action
-                ->setLabel("Sauvegarder puis ajouter un nouveau");
-            })
-            ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
-                return $action
-                ->setLabel('Créer et quitter');
-            })
-            ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, function (Action $action) {
-                return $action
-                ->setLabel("Sauvegarder puis quitter")
-                ->setIcon("fa-solid fa-floppy-disk");
-            })
-            ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
-               return $action->setIcon('fa-solid fa-eye');
-            });
+
+        return $actions
+        ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+            return $action->setIcon('fa-fw fas fa-person-snowboarding')->setLabel("Nouveau trick");
+        })
+        ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+            return $action
+            ->setIcon('fa-solid fa-gears');
+        })
+        ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+            return $action
+            ->setIcon('fa-solid fa-trash');
+        })
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER, function (Action $action) {
+            return $action
+            ->setLabel("Sauvegarder puis ajouter un nouveau");
+        })
+        ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
+            return $action
+            ->setLabel('Créer et quitter');
+        })
+        ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, function (Action $action) {
+            return $action
+            ->setLabel("Sauvegarder puis quitter")
+            ->setIcon("fa-solid fa-floppy-disk");
+        })
+        ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+            return $action->setIcon('fa-solid fa-eye');
+        });
 
         
     }
 
     public function configureFields(string $pageName): iterable
     {
+        if ($this->getUser()->getId() !== $this->trickRepository->findOneBy(['id'=> $this->getUser()->getId()])) {
+            $this->redirectToRoute('app_login');
+        }
+
         yield IdField::new('id')->hideOnForm()
         ->hideOnIndex();
+
         yield AssociationField::new('category')
         ->setLabel('Catégorie');
+
         yield TextField::new('name', 'Nom du trick');
+        yield SlugField::new('slug')->setTargetFieldName("name");
+
         yield TextareaField::new('Description');
 
-        yield SlugField::new('slug')->setTargetFieldName("name")
-        ->hideOnForm();
-        yield DateTimeField::new('updatedAt')->onlyOnIndex()->setLabel('mis à jour le');
-        yield DateTimeField::new('CreatedAt')->onlyOnIndex()->setLabel('date de création');
+        yield DateTimeField::new('updatedAt')->onlyOnIndex()->setLabel('mis à jour le')->hideOnForm();
+        yield DateTimeField::new('CreatedAt')->onlyOnIndex()->setLabel('date de création')->hideOnForm();
     }
 
     public function configureCrud(Crud $crud): Crud
