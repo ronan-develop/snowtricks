daab33cfbfe52c49e5863c30414b6a7457c930d9
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-15 15:27:44 +0200

    Refactor: broken Registration Submit in form Files: - src/Controller/RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use DateTimeZone;
 use App\Entity\User;
+use DateTimeImmutable;
 use App\Form\RegistrationFormType;
 use App\Security\AppAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
@@ -12,18 +14,38 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class RegistrationController extends AbstractController
 {
+
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserAuthenticatorInterface $userAuthenticator, AppAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        UserAuthenticatorInterface $userAuthenticator,
+        AppAuthenticator $authenticator,
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger
+        ): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
+
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $user->setCreatedAt(new DateTimeImmutable("now", new DateTimeZone('Europe/Paris')));
+            $user->setSlug(
+                strtolower($slugger->slug($user->getUsername()))
+            );
+
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
