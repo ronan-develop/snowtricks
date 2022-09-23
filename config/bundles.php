0e75e7d55aa928e1efc1211784e1c72187be930a
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-23 11:55:07 +0200

    Package: FOJsRoutingBundle docs: https://symfony.com/doc/4.0/routing/generate_url_javascript.html modified:   composer.json modified:   composer.lock modified:   config/bundles.php mew file:   config/routes/fos_js_routing.yaml modified:   package-lock.json modified:   package.json modified:   symfony.lock

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
 ];
