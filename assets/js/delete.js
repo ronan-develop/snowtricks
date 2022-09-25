f17976ea96620c66357d9fc08b781ecc8f06cc8f
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-25 17:42:02 +0200

    Modal : open close - Files : modified:   assets/js/delete.js: launch close modified:   assets/styles/_vars/_vars.scss: background - close button modified:   assets/styles/components/_modal.scss : design - animation modified:   templates/base.html.twig : remove class overlay modified:   templates/components/_modal.html.twig : modal template modified:   templates/components/_trick.html.twig : data-delete

--- assets/js/delete.js
+++ assets/js/delete.js
@@ -3,10 +3,42 @@
 document.querySelectorAll('a#delete-button').forEach((link)=>{
     link.addEventListener('click', launchModal);
 });
-window.addEventListener('load',() => {
-    document.querySelector("#modal").style = 'display: none;';
 
-});
+function launchModal(e){
+    e.preventDefault();
+    // handle attributes
+    const modal = document.getElementById("modal-trick");
+    modal.removeAttribute("style");
+    modal.setAttribute('aria-modal', 'true');
+    modal.removeAttribute('aria-hidden');
+    let link = this.dataset.delete;
+    let cancel = document.querySelector("#modal-trick > div > button.secondary.outline");
+    let confirm = document.querySelector("#modal-trick > div > button:nth-child(5)");
+
+    cancel.addEventListener('click', (e)=>{
+        console.log('cancel');
+        e.preventDefault();
+        closeModal();
+        cancel.removeEventListener("click", closeModal, true);
+        return;
+    });
+
+    confirm.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+    })
+}
+
+function closeModal() {
+    const modal = document.getElementById("modal-trick");
+    modal.setAttribute("style", "display: none;");
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+}
+
+function deleteTrick(link) {
+    console.log(this);
+}
 
 // function cancel() {
 //     console.log('cancel');
