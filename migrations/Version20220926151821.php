27347fcbb7e01417258cd66bb8c3f19b7c7fa99b
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-26 17:27:27 +0200

    Fixes: Thumbnail for trick in admin set params in services.yaml to bind var Files: modified:   config/packages/vich_uploader.yaml: URIprefix & upload destination modified:   config/services.yaml: uploadDir & bind deleted:    migrations/Version20220908171932.php deleted:    migrations/Version20220908172558.php deleted:    migrations/Version20220909061929.php deleted:    migrations/Version20220909075512.php deleted:    migrations/Version20220909083609.php deleted:    migrations/Version20220910065606.php deleted:    migrations/Version20220910121949.php deleted:    migrations/Version20220910161234.php deleted:    migrations/Version20220912085454.php deleted:    migrations/Version20220912114502.php deleted:    migrations/Version20220912114637.php deleted:    migrations/Version20220912114728.php deleted:    migrations/Version20220913130734.php deleted:    migrations/Version20220913131119.php deleted:    migrations/Version20220914083002.php deleted:    migrations/Version20220914131823.php deleted:    migrations/Version20220914132127.php deleted:    migrations/Version20220914135302.php deleted:    migrations/Version20220916065904.php deleted:    migrations/Version20220916084240.php renamed:    migrations/Version20220914131714.php -> migrations/Version20220926151821.php modified:   src/Controller/Admin/TrickCrudController.php: binding in constructor modified:   src/Entity/Trick.php: media 1-2-3

--- migrations/Version20220926151821.php
+++ migrations/Version20220926151821.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220914135302 extends AbstractMigration
+final class Version20220926151821 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20220914135302 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE trick ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('DROP INDEX UNIQ_D8F0A91E989D9B62 ON trick');
+        $this->addSql('ALTER TABLE trick ADD media1 VARCHAR(255) DEFAULT NULL, ADD media2 VARCHAR(255) DEFAULT NULL, ADD media3 VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE trick DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE trick DROP media1, DROP media2, DROP media3');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D8F0A91E989D9B62 ON trick (slug)');
     }
 }
