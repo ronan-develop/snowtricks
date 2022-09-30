e41889970347ac95f12342d2a9c3872c9f5f9f4d
Ronan Lenouvel <ronan.lenouvel@gmail.com>
2022-09-30 15:18:08 +0200

    Mail: send email on registration modified:   src/Controller/RegistrationController.php new file:   src/Services/MailerService.php new file:   templates/emails/signup.html.twig

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,17 +5,16 @@ namespace App\Controller;
 use DateTimeZone;
 use App\Entity\User;
 use DateTimeImmutable;
+use App\Services\MailerService;
 use App\Form\RegistrationFormType;
-use App\Security\AppAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -23,17 +22,14 @@ class RegistrationController extends AbstractController
     public function register(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasher,
-        UserAuthenticatorInterface $userAuthenticator,
-        AppAuthenticator $authenticator,
+        VerifyEmailHelperInterface $verifyEmailHelper,
         EntityManagerInterface $entityManager,
         SluggerInterface $slugger,
-        Session $session
+        MailerService $mailer
     ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
-
-        
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user->setPassword(
@@ -42,6 +38,7 @@ class RegistrationController extends AbstractController
                     $form->get('password')->getData()
                 )
             );
+
             $user->setCreatedAt(new DateTimeImmutable("now", new DateTimeZone('Europe/Paris')));
             $user->setSlug(
                 strtolower($slugger->slug($user->getUsername()))
@@ -51,15 +48,36 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
+            $this->addFlash('success', 'Vous devez confirmer votre compte, relevez vos emails (😉 dans le doute, vérifiez vos SPAMS)');
+
+
+            $signatureComponents = $verifyEmailHelper->generateSignature(
+                'app_verify_email',
+                $user->getId(),
+                $user->getEmail(),
+                ['id' => $user->getId()]
             );
+
+            $mailer->sendEmail(
+                to: $user->getEmail(),
+                subject: "Activation de votre compte Snowtricks",
+                body: 'Veuillez cliquer<a href='. $signatureComponents->getSignedUrl() .'> ici </a> pour activer votre compte. Expiration dans '.$signatureComponents->getExpiresAt()
+                ->format('d/m/y-H:i:s')
+            );
+
+            return $this->render('emails/signup.html.twig', [
+                'user' => $user
+            ]);
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/verify', name: 'app_verify_email')]
+    public function verifyUserEmail(): Response
+    {
+        return new Response();
     }
 }
